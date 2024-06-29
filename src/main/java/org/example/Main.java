958830dc71d04b7b0c7fa1d0e4554a1eaf156113
hoyo1744 <hoyo1744@naver.com>
2024-06-29 15:08:25 +0900

    https://www.acmicpc.net/problem/2869

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,40 +1,50 @@
 package org.example;
 
 
-import java.util.*;
+import java.util.Scanner;
 
 class Main {
 
-    public static int t;
-
     public static Scanner sc = new Scanner(System.in);
 
+    public static int v = 0;
 
+    public static int a = 0;
+
+    public static int b = 0;
+
+    public static int result = 0;
     public static void main(String[] args) {
 
+        input();
         solve();
-
+        output();
     }
 
     public static void solve() {
-        t = sc.nextInt();
 
-        for (int i = 0; i < t; i++) {
-            int c = sc.nextInt();
+        result = (v - b) / ( a- b);
 
-            int q = c/ 25;
-            c = c%25;
-            int d =  c/10;
-            c = c%10;
-            int n =  c/5;
-            c = c%5;
-            int p = c/1;
-            c = c%1;
+        int remain = ( v - b) % ( a- b);
+        if (remain == 0) {
+            result = (v - b) / (a - b);
+        } else {
+            result = (v - b) / (a - b) + 1;
 
-            System.out.println(q + " " + d + " " + n + " " + p);
         }
 
     }
+
+    public static void input() {
+        a = sc.nextInt();
+        b = sc.nextInt();
+        v = sc.nextInt();
+    }
+
+    public static void output() {
+        System.out.println(result);
+    }
+
 }
 
 
