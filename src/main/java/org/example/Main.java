fda1dac15ed7b5106c6ac663fdb756b0aa0c5b20
hoyo1744 <hoyo1744@naver.com>
2024-08-18 13:19:42 +0900

    브루트포스 시간 복잡도  = 2^(5000/3) = 2^1,666,666 = 시간 초과 따라서, 그리디 혹은 DP로 풀어야함 현 해결 방법 : DP

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,12 +4,18 @@ package org.example;
 // 설탕 배달
 // 완전탐색으로 풀 경우, 최대 2^1666의 시간복잡도를 갖는다.
 // 그래서 완전탐색은 안되고, 그리디로 풀어야함.
+// 두번째 : 다이나믹 프로그래밍
 
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 class Main {
+
+
+    public static final int INF = 99999;
+
 
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -19,20 +25,31 @@ class Main {
 
     public static int result = Integer.MAX_VALUE;
 
+    public static boolean[] visit = new boolean[10005];
+
+    //   dp[n] = n 키로 그램을 만들기 위해 필요한 최소한의 설탕 봉투 수.
+    public static int[] dp = new int[5001];
+
 
     public static void main(String[] args) throws IOException {
+        init();
         input();
         solve();
         output();
     }
 
+    public static void init() {
+        Arrays.fill(dp, INF);
+    }
     public static void output() throws IOException {
-        if (result == Integer.MAX_VALUE) {
-            wr.write("-1");
+
+        if (visit[n] == true) {
+            wr.write(String.valueOf(dp[n]));
         } else {
-            wr.write(String.valueOf(result));
+            wr.write("-1");
         }
         wr.flush();
+
     }
 
     public static void input() throws IOException {
@@ -42,24 +59,32 @@ class Main {
 
         n = Integer.parseInt(st.nextToken());
 
+
     }
 
     public static void solve() {
-        int num = n / 5;
 
+        for (int i = 3; i <= 5001; i += 3) {
+            visit[i] = true;
 
+            for (int j = 5; j <= 5001; j +=5) {
+                visit[j] = true;
+                visit[j + i] = true;
 
+            }
+        }
 
+        dp[3] = 1;
+        dp[5] = 1;
 
-        for (int i = num; i >= 0; i--) {
-            int div = n - i*5;
-            if (div % 3 == 0) {
-                result = i + div/3;
-                break;
+        for (int i = 6; i <= n; i++) {
+            if (i - 3 >= 0 || i - 5 >= 0) {
+                dp[i] = Integer.min(dp[i-3], dp[i-5]) + 1;
             }
         }
 
     }
+
 }
 
 
