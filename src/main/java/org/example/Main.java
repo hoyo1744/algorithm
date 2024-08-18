7ed2df8eacb5e6995fe7dd65ec5e075848e99ef8
hoyo1744 <hoyo1744@naver.com>
2024-08-18 17:39:42 +0900

    N과 M(3) [BT] problem/15651 [RV] N/A 시간복잡도 = n^m = 8^8 풀이방법 = 완전탐색(브루트포스)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,65 +1,65 @@
 package org.example;
 
 
-// n과 m(1)
+// n과 m(3)
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 class Main {
+
+    public static int n;
+    public static int m;
 
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static int n;
-
-    public static int m;
+    public static Scanner sc = new Scanner(System.in);
 
     public static List<Integer> list = new ArrayList<>();
 
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws IOException{
 
         input();
 
         for (int i = 1; i <= n; i++) {
-
             list.add(i);
             solve(i, 1);
             list.remove(list.size() - 1);
-
         }
+
     }
 
     public static void input() throws IOException {
+
         String in = br.readLine();
 
         StringTokenizer st = new StringTokenizer(in);
+
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
     }
 
     public static void solve(int num, int dist) {
+
         if (dist == m) {
             for (Integer i : list) {
-                System.out.print(i + " ");
+                System.out.print(i+ " ");
             }
             System.out.print("\n");
             return;
         }
 
-        for (int i = num + 1; i <= n; i++) {
-            if (dist + 1 <= m) {
-                list.add(i);
-                solve(i, dist + 1);
-                list.remove(list.size() - 1);
-            }
-
+        for (int i = 1; i <= n; i++) {
+            list.add(i);
+            solve(i, dist + 1);
+            list.remove(list.size() - 1);
         }
-
-
     }
 
 
