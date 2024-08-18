4553d27b544a44879679b8bfd738755d7847505c
hoyo1744 <hoyo1744@naver.com>
2024-08-18 18:01:59 +0900

    N과 M(4) [BT] problem/15652 [RV] N/A 시간복잡도 = n^m = 8^8 풀이방법 = 완전탐색(브루트포스) 특이사항 = BufferedWriter를 사용하지 않으면 시간초과가 발생함.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,29 +1,24 @@
 package org.example;
 
 
-// n과 m(3)
+// n과 m(4)
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.util.*;
+
 
 class Main {
 
     public static int n;
+
     public static int m;
-
+    public static Scanner sc = new Scanner(System.in);
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static Scanner sc = new Scanner(System.in);
+    public static ArrayList<Integer> list = new ArrayList<>();
 
-    public static List<Integer> list = new ArrayList<>();
-
-
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
 
         input();
 
@@ -36,20 +31,18 @@ class Main {
         bw.flush();
 
 
-
     }
 
     public static void input() throws IOException {
-
         String in = br.readLine();
 
         StringTokenizer st = new StringTokenizer(in);
-
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
+
     }
 
-    public static void solve(int num, int dist) throws IOException{
+    public static void solve(int num, int dist) throws IOException {
 
         if (dist == m) {
             for (Integer i : list) {
@@ -58,20 +51,18 @@ class Main {
             }
             bw.write("\n");
 
-
             return;
         }
 
-        for (int i = 1; i <= n; i++) {
+        for (int i = num; i <= n; i++) {
             if (dist + 1 <= m) {
                 list.add(i);
                 solve(i, dist + 1);
                 list.remove(list.size() - 1);
             }
         }
+
     }
-
-
 }
 
 
