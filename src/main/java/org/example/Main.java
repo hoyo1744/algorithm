4c15ec011b69e8d4563236af8f2c265b8ae60730
hoyo1744 <hoyo1744@naver.com>
2024-08-27 19:44:10 +0900

    order 정리 [BT] N/A [RV] N/A 컬렉션 정렬 정리

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,10 @@
 package org.example;
 
 
+// 알고리즘 기본 import
+
 import java.io.*;
 import java.util.*;
-
 
 
 class Main {
@@ -12,8 +13,10 @@ class Main {
 
     public static int m;
 
-
+    //   BufferedReader 선언
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    //    BufferedWriter 선언
     public static BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static ArrayList<Integer> arr = new ArrayList();
@@ -23,28 +26,23 @@ class Main {
     public static boolean[] visit = new boolean[10005];
 
     public static void main(String[] args) throws IOException {
-        input();
+//        input();
+        orderArrayList();
+        orderCollections();
+        orderUserCustomUsingComparable();
+        orderUserCustomUsingComparator();
 
-
-        for (int i = 0; i < arr.size(); i++) {
-            visit[arr.get(i)] = true;
-            result.add(arr.get(i));
-            solve(1);
-            visit[arr.get(i)] = false;
-            result.remove(result.size() - 1);
-        }
-
-        wr.flush();
 
     }
 
+    //    BufferedReader를 이용한 Read방법
     public static void input() throws IOException {
 
         String str = br.readLine();
 
         StringTokenizer st = new StringTokenizer(str);
         n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt( st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
         str = br.readLine();
         st = new StringTokenizer(str);
@@ -57,35 +55,135 @@ class Main {
 
     }
 
-    public static void solve(int count) throws IOException{
+    public static void orderArrayList() throws IOException {
 
-        if (count == m) {
+        System.out.println("###OrderArrayList##");
 
-            for (int i = 0; i < result.size(); i++) {
 
-                wr.write(String.valueOf(result.get(i)));
-                wr.write(" ");
-            }
-            wr.write("\n");
-            return;
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+
+
+        // ArrayList에서의 오름차순 정렬
+        list.sort(Comparator.naturalOrder());
+        for (Integer integer : list) {
+            System.out.print(integer + " ");
         }
+        System.out.print("\n");
 
-        for (int i = 0; i < n; i++) {
-            if (visit[arr.get(i)] == false) {
-                visit[arr.get(i)] = true;
-                result.add(arr.get(i));
-                solve(count + 1);
-                visit[arr.get(i)] = false;
-                result.remove(result.size() - 1);
-            }
+        // ArrayList에서의 내림차순 정렬
+        list.sort(Comparator.reverseOrder());
+        for (Integer integer : list) {
+            System.out.print(integer + " ");
         }
+        System.out.print("\n");
+    }
 
+    public static void orderCollections() {
 
+        System.out.println("###orderCollections##");
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+
+//        Collections를 이용한 오름차순 정렬
+        Collections.sort(list);
+        for (Integer integer : list) {
+            System.out.print(integer + " ");
+        }
+        System.out.print("\n");
+
+//        Collections를 이용한 내림차순 정렬
+        Collections.sort(list, Collections.reverseOrder());
+        for (Integer integer : list) {
+            System.out.print(integer + " ");
+        }
+        System.out.print("\n");
 
 
     }
 
+    public static void orderUserCustomUsingComparable() {
 
+        System.out.println("###orderUserCustomUsingComparable##");
+        ArrayList<Fruit> list = new ArrayList<>();
+
+        list.add(new Fruit("Apple", 2000));
+        list.add(new Fruit("Orange", 3000));
+        list.add(new Fruit("Banana", 1000));
+
+//        Collections는 내부에서 정렬시 Comparable의 compareTo를 이용함.
+//        사용자 정의 오름차순 정렬
+//        Collections.sort(list);
+        list.sort(Comparator.naturalOrder());
+        System.out.println(list);
+
+//        사용자 정의 내림차순 정렬
+//        Collections.sort(list, Collections.reverseOrder());
+        list.sort(Comparator.reverseOrder());
+        System.out.println(list);
+
+
+    }
+
+    public static void orderUserCustomUsingComparator() {
+        System.out.println("###orderUserCustomUsingComparator##");
+        ArrayList<Fruit> list = new ArrayList<>();
+
+        list.add(new Fruit("Apple", 2000));
+        list.add(new Fruit("Orange", 3000));
+        list.add(new Fruit("Banana", 1000));
+
+
+        // 오름차순
+        list.sort(new FruitPrice());
+        System.out.println(list);
+
+        // 내림차순
+        list.sort(new FruitPrice().reversed());
+        System.out.println(list);
+
+
+    }
+
+    static class FruitPrice implements Comparator<Fruit> {
+
+        @Override
+        public int compare(Fruit f1, Fruit f2) {
+            if (f1.price > f2.price) {
+                return 1;
+            } else if (f1.price < f2.price) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+    }
+
+
+    static class Fruit implements Comparable<Fruit> {
+
+        private String name;
+        private int price;
+
+        public Fruit(String name, int price) {
+            this.name = name;
+            this.price = price;
+        }
+
+        @Override
+        public int compareTo(Fruit fruit) {
+            if (fruit.price < price) {
+                return 1;
+            } else if (fruit.price > price) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+
+        @Override
+        public String toString() {
+            return "[" + this.name + ": " + this.price + " ]";
+        }
+    }
 
 
 }
