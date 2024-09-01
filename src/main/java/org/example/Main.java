0fbb2cf4949de01728a37745d9f58456e62dc01a
hoyo1744 <hoyo1744@naver.com>
2024-09-01 15:45:38 +0900

    수 정렬하기 3 [BT] problem/10989 [RV] N/A 정렬, 메모리 제한이 8MB

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,11 +13,18 @@ class Main {
 
     public static int n;
 
-    public static ArrayList<Integer> list = new ArrayList<>();
 
-    public static void main(String[] args) throws IOException{
+    public static int[] arr = new int[10005];
 
+    public static void main(String[] args) throws IOException {
+
+        init();
         input();
+        solve();
+    }
+
+    public static void init() throws IOException {
+        Arrays.fill(arr, 0);
     }
 
     public static void input() throws IOException {
@@ -26,27 +33,33 @@ class Main {
 
         StringTokenizer st = new StringTokenizer(str);
 
+
         n = Integer.parseInt(st.nextToken());
 
         for (int i = 0; i < n; i++) {
-
             str = br.readLine();
+
             st = new StringTokenizer(str);
 
             int next = Integer.parseInt(st.nextToken());
 
-            list.add(next);
+            arr[next]++;
         }
+    }
 
-        list.sort(Comparator.naturalOrder());
+    public static void solve() throws IOException {
+        for (int i = 1; i <= 10000; i++) {
 
+            int cnt = arr[i];
 
-        for (int i = 0; i < n; i++) {
-            bw.write(String.valueOf(list.get(i)));
-            bw.write("\n");
+            for (int j = 0; j < cnt; j++) {
+                bw.write(String.valueOf(i));
+                bw.write("\n");
+            }
         }
 
         bw.flush();
+
     }
 
 }
