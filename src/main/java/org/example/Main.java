2f7b99adcca11a805a4e2ff743efab713ef183ee
hoyo1744 <hoyo1744@naver.com>
2024-09-14 14:52:35 +0900

    나이순 정렬 [BT] problem/10814 [RV] N/A 정렬

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,58 +6,72 @@ import java.io.*;
 
 class Main {
 
-    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static HashMap<String, Boolean> check = new HashMap<>();
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static int n;
 
-    public static ArrayList<Str> list = new ArrayList<>();
+    public static ArrayList<Person> list = new ArrayList<>();
 
-    public static class Str implements Comparable<Str>{
-        String msg;
 
-        public Str(String msg) {
-            this.msg = msg;
+    public static class Person implements Comparable<Person>{
+
+        int age;
+        String name;
+        int seq;
+
+        public Person(int age, String name, int seq){
+            this.age = age;
+            this.name = name;
+            this.seq = seq;
+        }
+
+
+        @Override
+        public int compareTo(Person p){
+            if ( this.age == p.age){
+                return this.seq - p.seq;
+            }
+
+            return this.age - p.age;
         }
 
         @Override
-        public int compareTo(Str s1) {
-
-            if (this.msg.length() == s1.msg.length()) {
-                return this.msg.compareTo(s1.msg);
-            }
-
-            return this.msg.length() - s1.msg.length();
-
+        public String toString(){
+            return this.age + " " + this.name;
         }
-
     }
 
 
-    public static void main(String[] args)  throws IOException{
+
+    public static void  main(String args[]) throws IOException{
 
         input();
         solve();
         output();
+
     }
 
-    public static void input() throws IOException {
-
+    public static void input() throws IOException{
         String str = br.readLine();
-        StringTokenizer st  = new StringTokenizer(str);
 
-        n = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(str);
 
         for (int i = 0; i < n; i++) {
+
             str = br.readLine();
-            st = new StringTokenizer(str);
 
-            String msg = st.nextToken();
-            Str temp = new Str(msg);
+            StringTokenizer st = new StringTokenizer(str);
 
-            list.add(temp);
+            int age = Integer.parseInt(st.nextToken());
+            int seq = i;
+            String name = st.nextToken();
+
+            Person person = new Person(age, name, seq);
+
+            list.add(person);
         }
     }
 
@@ -65,20 +79,13 @@ class Main {
         list.sort(Comparator.naturalOrder());
     }
 
-    public static void output() throws IOException{
-
+    public static void output() throws IOException {
         for (int i = 0; i < list.size(); i++) {
-            Boolean c = check.get(list.get(i).msg);
-            if (c == null) {
-                bw.write(list.get(i).msg);
-                bw.write("\n");
-            }
-
-            check.put(list.get(i).msg, true);
+            bw.write(list.get(i).toString());
+            bw.write("\n");
         }
 
         bw.flush();
-
     }
 
 
