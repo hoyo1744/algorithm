692dd183e44b002c3bdc4674069b8112fe2dbe8c
hoyo1744 <hoyo1744@naver.com>
2024-09-15 16:01:07 +0900

    좌표 압축 [BT] problem/18870 [RV] N/A 정렬

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,52 +1,29 @@
 package org.example;
 
 
+
 import java.util.*;
 import java.io.*;
 
 class Main {
 
+    public static int n;
+
+    public static ArrayList<Integer> list = new ArrayList<>();
+
+    public static ArrayList<Integer> listNoDuplicated = new ArrayList<>();
+
+    public static HashMap<Integer, Boolean> duplicated = new HashMap<>();
 
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static int n;
-
-    public static ArrayList<Person> list = new ArrayList<>();
-
-
-    public static class Person implements Comparable<Person>{
-
-        int age;
-        String name;
-        int seq;
-
-        public Person(int age, String name, int seq){
-            this.age = age;
-            this.name = name;
-            this.seq = seq;
-        }
-
-
-        @Override
-        public int compareTo(Person p){
-            if ( this.age == p.age){
-                return this.seq - p.seq;
-            }
-
-            return this.age - p.age;
-        }
-
-        @Override
-        public String toString(){
-            return this.age + " " + this.name;
-        }
-    }
+    public static HashMap<Integer, Integer> result = new HashMap<>();
 
 
 
-    public static void  main(String args[]) throws IOException{
+    public static void main(String[] args) throws IOException{
 
         input();
         solve();
@@ -54,39 +31,82 @@ class Main {
 
     }
 
-    public static void input() throws IOException{
+
+    public static void input() throws  IOException {
         String str = br.readLine();
 
         n = Integer.parseInt(str);
 
+        str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str);
+
         for (int i = 0; i < n; i++) {
+            int value = Integer.parseInt(st.nextToken());
 
-            str = br.readLine();
+            if(Objects.isNull(duplicated.get(value))){
+                duplicated.put(value, true);
+                listNoDuplicated.add(value);
+            }
 
-            StringTokenizer st = new StringTokenizer(str);
-
-            int age = Integer.parseInt(st.nextToken());
-            int seq = i;
-            String name = st.nextToken();
-
-            Person person = new Person(age, name, seq);
-
-            list.add(person);
+            list.add(value);
         }
+    }
+
+    public static int binarySearch(int value) {
+
+        int left = 1;
+        int right = listNoDuplicated.size() - 1;
+
+        int ret = 0;
+
+        while (left <= right) {
+
+            int mid = left + ( right - left) / 2;
+
+            if (listNoDuplicated.get(mid) == value) {
+                ret =  mid;
+                break;
+            }
+
+            if (listNoDuplicated.get(mid) < value) {
+                left = mid + 1;
+            } else {
+
+                right = mid - 1;
+
+            }
+        }
+
+        return ret;
     }
 
     public static void solve() {
-        list.sort(Comparator.naturalOrder());
+
+        // 1. 중복제거된 리스트 정렬
+        listNoDuplicated.sort(Comparator.naturalOrder());
+
+        // 2. 중복제거된 리스트에 대해서 이분탐색 결과 : 정답
+
+        for (int i = 0; i < listNoDuplicated.size(); i++) {
+            //int idx = binarySearch(listNoDuplicated.get(i));
+            result.put(listNoDuplicated.get(i), i);
+        }
+
     }
 
     public static void output() throws IOException {
+
         for (int i = 0; i < list.size(); i++) {
-            bw.write(list.get(i).toString());
-            bw.write("\n");
+            int count = result.get(list.get(i));
+
+            bw.write(String.valueOf(count));
+            bw.write(" ");
         }
 
         bw.flush();
+
     }
+
 
 
 
