167c325f114e3ca49c5ce33b915e9e6ec9b2a163
hoyo1744 <hoyo1744@naver.com>
2024-09-16 15:05:09 +0900

    문자열 집합 [BT] problem/14425 [RV] N/A 집합

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,70 +1,54 @@
 package org.example;
 
 
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 
 class Main {
 
-
     public static int n;
-
     public static int m;
 
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static HashMap<Integer, Boolean> check = new HashMap<>();
+    public static int result;
 
-    public static ArrayList<Integer> result = new ArrayList<>();
+    public static HashMap<String, Boolean> s = new HashMap<>();
+
 
     public static void main(String[] args) throws IOException {
 
         String str = br.readLine();
 
-        n = Integer.parseInt(str);
-
-        str = br.readLine();
-
         StringTokenizer st = new StringTokenizer(str);
 
-        while (st.hasMoreTokens()) {
-            check.put(Integer.parseInt(st.nextToken()), true);
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < n; i++) {
+            str = br.readLine();
+
+            s.put(str, true);
         }
 
+        for (int i = 0; i < m; i++) {
+            str = br.readLine();
 
-
-        str = br.readLine();
-        m = Integer.parseInt(str);
-
-        str = br.readLine();
-
-        st = new StringTokenizer(str);
-
-        while(st.hasMoreTokens()){
-            int value = Integer.parseInt(st.nextToken());
-
-            if (check.get(value) != null) {
-                result.add(1);
-            } else {
-                result.add(0);
+            if (s.get(str) != null) {
+                result++;
             }
+
         }
 
-
-        for (int i = 0; i < result.size(); i++) {
-            bw.write(String.valueOf(result.get(i)) + " ");
-        }
+        bw.write(String.valueOf(result));
 
         bw.flush();
 
-
-
-
-
-
     }
+
 
 }
 
