4343a2329dd32ed9274e0b72a82e5a31aeb4e65f
hoyo1744 <hoyo1744@naver.com>
2024-09-19 14:53:55 +0900

    듣보잡 [BT] problem/1764 [RV] N/A 집합

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,71 +2,90 @@ package org.example;
 
 
 
-import java.util.*;
 import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
 
 
 class Main {
 
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static int n;
+
     public static int m;
 
-    public static HashMap<Integer, Integer> datas = new HashMap<>();
+    public static HashMap<String, Integer> check = new HashMap<>();
 
+    public static ArrayList<String> resultName = new ArrayList<>();
+
+    public static int resultCount;
 
 
 
     public static void main(String[] args) throws IOException{
 
         String str = br.readLine();
-        n = Integer.parseInt(str);
 
-
-        str = br.readLine();
         StringTokenizer st = new StringTokenizer(str);
+
+        n = Integer.parseInt(st.nextToken());
+
+        m = Integer.parseInt(st.nextToken());
+
+
         for (int i = 0; i < n; i++) {
+            str = br.readLine();
+            check.put(str, 1);
+        }
 
-            int num = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < m; i++) {
+            str = br.readLine();
 
-            Integer count = datas.get(num);
+
+            Integer count = check.get(str);
 
             if (count == null) {
-                datas.put(num, 1);
+                check.put(str, 1);
             } else {
-                datas.put(num, count+1);
+                check.put(str, count + 1);
             }
         }
 
-        str = br.readLine();
+        Set<String> keys = check.keySet();
 
-        m = Integer.parseInt(str);
+        List<String> keyList = keys.stream().collect(Collectors.toList());
 
-        str = br.readLine();
-        st = new StringTokenizer(str);
 
-        for (int i = 0; i < m; i++) {
-            int num = Integer.parseInt(st.nextToken());
 
-            Integer data = datas.get(num);
+        for (int i = 0; i < keyList.size(); i++) {
+            String key = keyList.get(i);
 
-            if (data == null) {
-                bw.write("0");
-            } else {
-                bw.write(String.valueOf(data));
+            if (check.get(key) >= 2) {
+                resultName.add(key);
             }
-            bw.write(" ");
+        }
+
+        resultCount = resultName.size();
+
+
+        resultName.sort(Comparator.naturalOrder());
+
+        bw.write(String.valueOf(resultCount));
+        bw.write("\n");
+
+        for (int i = 0; i < resultCount; i++) {
+
+            bw.write(resultName.get(i));
+            bw.write("\n");
         }
 
         bw.flush();
 
 
+
     }
-
-
 
 
 }
