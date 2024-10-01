2cda1c3b270789dcfbed51940567274818e58c3d
hoyo1744 <hoyo1744@naver.com>
2024-10-01 14:18:42 +0900

    스택 2 [BT] problem/28278 [RV] N/A stack

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,31 +6,66 @@ import java.util.*;
 import java.util.stream.*;
 import java.io.*;
 
-
 class Main {
 
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+    public static int n;
+
+    public static Stack<Integer> stack = new Stack<>();
 
     public static void main(String[] args) throws IOException{
 
         String str = br.readLine();
 
-        int n = Integer.parseInt(str);
+        n = Integer.parseInt(str);
 
-        int result = (int)Math.sqrt(n);
+        for (int i = 0; i < n; i++) {
+            String cmd = br.readLine();
+
+            StringTokenizer st = new StringTokenizer(cmd);
+            int num = Integer.parseInt(st.nextToken());
 
 
+            if (num == 5) {
+                if (stack.isEmpty()) {
+                    bw.write("-1\n");
+                } else {
+                    bw.write(String.valueOf(stack.peek()));
+                    bw.write("\n");
+                }
+            } else if (num == 4) {
+                if (stack.empty()) {
+                    bw.write("1\n");
+                } else {
+                    bw.write("0\n");
+                }
+            } else if (num == 3) {
+                bw.write(String.valueOf(stack.size()));
+                bw.write("\n");
+            } else if (num == 2) {
+                if (stack.empty()) {
+                    bw.write("-1\n");
+                } else {
+                    bw.write(String.valueOf(stack.pop()));
+                    bw.write("\n");
+                }
 
-        bw.write(String.valueOf(result));
+            } else if (num == 1) {
+                int input = Integer.parseInt(st.nextToken());
+                stack.push(input);
+            }
+        }
+
 
         bw.flush();
 
 
 
     }
+
 
 }
 
