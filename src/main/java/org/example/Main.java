a27a1721c1e8d69618834023baa60070e111ca83
hoyo1744 <hoyo1744@naver.com>
2024-10-09 21:05:38 +0900

    풍선 터뜨리기 [BT] problem/2346 [RV] N/A deque

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,6 @@
 package org.example;
 
 
-
 import java.util.*;
 import java.util.stream.*;
 import java.io.*;
@@ -13,77 +12,69 @@ class Main {
 
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static Deque<Integer> dq = new ArrayDeque<>();
+    public static Deque<Pair> dq = new ArrayDeque<>();
 
+    public static int n;
 
-    public static void main(String[] args) throws IOException{
+    public static class Pair{
+        public int index;
+        public int value;
 
-
-        int n = Integer.parseInt(br.readLine());
-
-
-        for (int i = 0; i < n; i++) {
-            String cmd = br.readLine();
-
-            StringTokenizer st = new StringTokenizer(cmd);
-            int num = Integer.parseInt(st.nextToken());
-
-            if (num == 1) {
-                int value = Integer.parseInt(st.nextToken());
-                dq.addFirst(value);
-            } else if (num == 2) {
-                int value = Integer.parseInt(st.nextToken());
-                dq.addLast(value);
-            } else if (num == 3) {
-                if (dq.isEmpty()) {
-                    bw.write("-1\n");
-                } else {
-                    bw.write(String.valueOf(dq.removeFirst()));
-                    bw.write("\n");
-                }
-            } else if (num == 4) {
-                if (dq.isEmpty()) {
-                    bw.write("-1\n");
-                } else {
-                    bw.write(String.valueOf(dq.removeLast()));
-                    bw.write("\n");
-                }
-            } else if (num == 5) {
-                bw.write(String.valueOf(dq.size()));
-                bw.write("\n");
-            } else if (num == 6) {
-                if (dq.isEmpty()) {
-                    bw.write("1\n");
-                } else {
-                    bw.write("0\n");
-                }
-            } else if (num == 7) {
-                if (dq.isEmpty()) {
-                    bw.write("-1\n");
-                } else {
-                    bw.write(String.valueOf(dq.peekFirst()));
-                    bw.write("\n");
-                }
-            } else if (num == 8) {
-                if (dq.isEmpty()) {
-                    bw.write("-1\n");
-                } else {
-                    bw.write(String.valueOf(dq.peekLast()));
-                    bw.write("\n");
-                }
-            }
-
-
-
+        Pair(int index, int value) {
+            this.index = index;
+            this.value = value;
         }
 
-
-//        System.out.println("-----------");
-        bw.flush();
 
     }
 
 
+    public static void main(String[] args) throws IOException{
 
+        n = Integer.parseInt(br.readLine());
+
+        String str = br.readLine();
+
+        StringTokenizer st = new StringTokenizer(str);
+
+        for (int i = 0; i < n; i++) {
+            dq.addLast(new Pair(i + 1, Integer.parseInt(st.nextToken())));
+        }
+
+        Pair value = dq.removeFirst();
+        bw.write(String.valueOf(value.index) + " ");
+
+
+        while (true) {
+
+            if (dq.isEmpty()) {
+                break;
+            }
+
+            if (value.value > 0) {
+                for (int i = 1; i < value.value; i++) {
+                    dq.addLast(dq.removeFirst());
+                }
+            } else {
+                for (int i = 1; i < value.value * -1; i++) {
+                    dq.addFirst(dq.removeLast());
+                }
+            }
+
+            if (value.value > 0) {
+                value = dq.removeFirst();
+            } else {
+                value = dq.removeLast();
+            }
+
+
+            bw.write(String.valueOf(value.index) + " ");
+        }
+
+        bw.flush();
+
+
+
+    }
 }
 
