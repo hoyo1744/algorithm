1526b6a791099f4b64115f01ba5a022885985fbb
hoyo1744 <hoyo1744@naver.com>
2024-10-19 16:39:36 +0900

    팩토리얼 [BT] problem/24723 [RV] N/A dp를 이용한 풀이

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,48 +7,31 @@ import java.io.*;
 
 class Main {
 
-    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+    public static int[] dp = new int[20];
 
-    public static int[][] dp = new int[10][10];
+    public static void main(String[] args) throws IOException{
 
-
-    public static void main(String[] args) throws IOException {
-
-
-        for (int i = 0; i < 10; i++) {
-            Arrays.fill(dp[i], 0);
-        }
+        Arrays.fill(dp, 0 );
 
         int n = Integer.parseInt(br.readLine());
 
-        dp[1][1] = 1;
+        dp[0] = 1;
 
-        for (int i = 2; i <= n+1; i++) {
-            for (int j = 1; j <= n+1; j++) {
-                if (j == 1 || j == n+1) {
-                    dp[i][j] = 1;
-                } else {
-                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
-                }
-            }
+        for (int i = 1; i <= n; i++) {
+            dp[i] = dp[i-1] * i;
         }
 
-
-        int result = 0;
-
-        for (int i = 1; i <= n + 1; i++) {
-            result += dp[n+1][i];
-        }
-
-
-        bw.write(String.valueOf(result));
+        bw.write(String.valueOf(dp[n]));
 
         bw.flush();
 
 
+
     }
+
 }
 
