e4cf85a3f43f392f968fa18901cd79b23929404a
hoyo1744 <hoyo1744@naver.com>
2024-11-03 13:39:37 +0900

    재귀의 귀재 [BT] problem/25501 [RV] N/A - 재귀

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,37 +1,56 @@
 package org.example;
 
-
-import java.util.*;
 import java.util.stream.*;
+import java.util.*;
 import java.io.*;
 
 class Main {
+
 
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static int[] dp = new int[22];
+    public static int t;
+
+    public static int cnt;
+
+
+
+    public static int solve(String str, int left, int right) {
+        cnt = cnt + 1;
+
+        if (left >= right) {
+            return 1;
+        } else if (str.charAt(left) != str.charAt(right)) {
+            return 0;
+        } else {
+            return solve(str, left + 1, right - 1);
+        }
+    }
+
 
     public static void main(String[] args) throws IOException{
 
-        int n = Integer.parseInt(br.readLine());
+        t = Integer.parseInt(br.readLine());
 
-        dp[0] = 0;
-        dp[1] = 1;
-
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+        for (int i = 0; i < t; i++) {
+            String str = br.readLine();
+            cnt = 0;
+            int result = solve(str, 0, str.length() - 1);
+            bw.write(String.valueOf(result) + " " + String.valueOf(cnt) + "\n");
         }
 
-        bw.write(String.valueOf(dp[n]));
         bw.flush();
 
 
 
 
 
+
     }
+
+
 
 
 }
