57e7f756fbb813e4aa4cc2043cf73e7e196621c8
hoyo1744 <hoyo1744@naver.com>
2024-11-04 16:15:23 +0900

    칸토어 집합 [BT] problem/4779 [RV] N/A - 분할정복

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
 import java.util.*;
+import java.util.stream.*;
 import java.io.*;
+
+
 
 class Main {
 
@@ -9,96 +12,44 @@ class Main {
 
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static int n;
-
-    public static int k;
-
-    public static int[] arr = new int[500005];
-
-    public static int[] sorted = new int[500005];
-
-    public static int cnt;
-
-    public static int result = -1;
-
-    public static void merge(int left, int mid, int right) {
-        int i = left;
-        int j = mid + 1;
-        int idx = left;
 
 
-        while (i <= mid && j <= right) {
-            if (arr[i] <= arr[j]) {
-                sorted[idx++] = arr[i++];
+    public static void solve(int left, int right, boolean mid) throws IOException{
+
+        if (right - left + 1 == 1 ) {
+            if (mid == false) {
+                if (left % 3 == 0 || left % 3 == 1) {
+                    bw.write("-");
+                } else {
+                    bw.write(" ");
+                }
             } else {
-                sorted[idx++] = arr[j++];
-            }
-        }
-
-
-        if (i > mid) {
-            for (int l = j; l <= right; l++) {
-                sorted[idx++] = arr[l];
-            }
-        } else {
-            for (int l = i; l <= mid; l++) {
-                sorted[idx++] = arr[l];
-            }
-        }
-
-        for (int l = left; l <= right; l++) {
-
-            arr[l] = sorted[l];
-
-            cnt += 1;
-            if (cnt == k) {
-                result = arr[l];
+                bw.write(" ");
             }
 
-        }
-    }
-
-    public static void merge_sort(int left, int right) {
-
-        int mid = 0;
-
-        if (left < right) {
-            mid =( left + right )/ 2;
-
-            merge_sort(left, mid);
-            merge_sort(mid + 1, right);
-            merge(left, mid, right);
+            return;
         }
 
+        int remain = (right - left + 1)/3;
 
-
-
+        solve(left, left + remain -1, mid);
+        solve(left + remain , left + remain + remain - 1, true);
+        solve( left + remain + remain, right, mid);
     }
 
     public static void main(String[] args) throws IOException{
 
-        String str = br.readLine();
-
-        StringTokenizer st = new StringTokenizer(str);
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-
-        str = br.readLine();
-        st = new StringTokenizer(str);
-        for (int i = 0; i < n; i++) {
-            int value = Integer.parseInt(st.nextToken());
-            arr[i] = value;
+        String str = "";
+        while ((str = br.readLine()) != null && !str.isEmpty()) {
+            int n = Integer.parseInt(str);
+            int last = (int)Math.pow(3, n);
+            solve(1, last, false);
+            bw.write("\n");
+            bw.flush();
         }
 
-
-        merge_sort(0, n - 1);
-
-        bw.write(String.valueOf(result));
-        bw.flush();
-
-
-
     }
+
 
 
 }
