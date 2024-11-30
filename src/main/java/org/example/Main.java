5aede976e493a193aef5b6afd8317f5ff03a59c8
hoyo1744 <hoyo1744@naver.com>
2024-11-30 15:04:03 +0900

    N-Queen [BT] problem/9663 [RV] N/A - 완전탐색

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,50 +1,66 @@
 package org.example;
 
-
-import java.util.*;
-import java.util.stream.*;
 import java.io.*;
+import java.util.*;
 
 
 class Main {
+
+    public static int n;
 
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static int n;
+    public static int[] queen = new int[20];
 
     public static int result;
 
-    public static void solve(int num, int left, int mid, int right) throws  IOException{
-        if (num == 1) {
-            bw.write(String.valueOf(left) + " " + String.valueOf(right) + "\n");
-            return;
-        }
+    public static Deque<Integer> history = new ArrayDeque<>();
 
-
-        solve(num - 1, left, right ,mid);
-        bw.write(String.valueOf(left) + " " + String.valueOf(right) + "\n");
-        solve(num - 1, mid, left, right);
-
+    public static void input() throws IOException {
+        n = Integer.parseInt(br.readLine());
     }
 
-    public static void main(String[] args) throws IOException{
-
-        n = Integer.parseInt(br.readLine());
-
-        result = 1;
-
-        for (int i = 1; i < n; i++) {
-            result = result * 2 + 1;
-        }
-
-        bw.write(String.valueOf(result) + "\n");
-
-        solve(n, 1, 2, 3);
+    public static void output() throws IOException {
+        bw.write(String.valueOf(result));
         bw.flush();
     }
 
+    public static boolean isQueenPossible(int row, int nextQueen) {
+
+        for (int col = 1; col <= row; col++) {
+
+            if (queen[col] == nextQueen || Math.abs(queen[col] - nextQueen) == (row - col)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void solve(int row, int cnt) {
+        if (cnt == n) {
+            result += 1;
+            return;
+        }
+
+        for (int col = 1; col <= n; col++) {
+            if (isQueenPossible(row, col )) {
+                queen[row] = col;
+                history.addLast(col);
+                solve(row + 1, cnt + 1);
+                queen[row] = 0;
+                history.removeLast();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        input();
+        solve(1, 0);
+        output();
+    }
 
 
 }
