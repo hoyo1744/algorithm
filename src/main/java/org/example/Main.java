7d9ff7391a34600477240c349d4e74ef0520d265
hoyo1744 <hoyo1744@naver.com>
2025-04-18 16:32:35 +0900

    ATM [BT] problem/11399 [RV] N/A - 구현

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,59 +1,70 @@
 package org.example;
 
+
 import java.util.*;
 import java.io.*;
 import java.util.stream.*;
 public class Main {
 
-
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+    public static int n;
+
+    public static ArrayList<Atm> list = new ArrayList<>();
+
     public static int result;
-    public static void main(String[] args) throws IOException {
 
+    static class Atm implements Comparable<Atm>{
+        public int num;
 
-        int n = Integer.parseInt(br.readLine());
+        public int p;
 
-
-        for (int i = 1; i <= n; i++) {
-            if (validation(i)) {
-                result++;
-            }
+        public Atm(int num, int p) {
+            this.num = num;
+            this.p = p;
         }
 
+        // 오름차순
+        @Override
+        public int compareTo(Atm atm){
+           return this.p - atm.p;
+        }
+    }
+
+    public static void input() throws IOException {
+        n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            int v = Integer.parseInt(st.nextToken());
+
+            list.add(new Atm(i, v));
+        }
+    }
+
+    public static void solve() {
+        list.sort(Comparator.naturalOrder());
+
+        int sum = 0;
+        for(int i = 0; i < list.size(); i++){
+            result += sum + list.get(i).p;
+            sum += list.get(i).p;
+        }
+    }
+
+    public static void output() throws IOException {
         bw.write(String.valueOf(result));
         bw.flush();
     }
 
-    public static boolean validation(int number){
 
-        ArrayList<Integer> digit = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
 
-        int temp = number;
-        while(temp > 0){
-            digit.add(temp%10);
-            temp /= 10;
-        }
-
-        if ( digit.size() == 1) {
-            return true;
-        }
-
-        if (digit.size() == 2) {
-            return true;
-        }
-
-        if (digit.size() == 3) {
-            if ( digit.get(0) - digit.get(1) == digit.get(1) - digit.get(2) ||
-                    digit.get(2) - digit.get(1) == digit.get(1) - digit.get(0)
-            ){
-                return true;
-            }
-        }
-
-        return false;
+        input();
+        solve();
+        output();
     }
 
 
