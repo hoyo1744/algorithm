51ba8ea7145bc950436dd4c5378460a0f027a950
hoyo1744 <hoyo1744@naver.com>
2025-04-18 23:13:34 +0900

    DFS와 BFS [BT] problem/1260 [RV] N/A - 구현

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,116 +3,110 @@ package org.example;
 
 
 import java.util.*;
+import java.io.*;
 import java.util.stream.*;
 
-import java.io.*;
-
 public class Main {
-
-    public static int n;
 
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static ArrayList<Person> list = new ArrayList<>();
+    public static int n;
 
-    public static HashMap<Integer, Integer> map = new HashMap<>();
+    public static int m;
 
+    public static int v;
 
+    public static boolean[][] arr = new boolean[1001][1001];
 
-    static class Person implements Comparable<Person>{
-        public int weight;
-
-        public int height;
-
-        public int num;
-
-        public Person(int w, int h, int num) {
-            this.weight = w;
-            this.height = h;
-            this.num = num;
-        }
-
-        @Override
-        public int compareTo(Person p) {
-            if (this.weight > p.weight && this.height > p.height) {
-                return 1;
-            } else if (this.weight == p.weight && this.height == p.height) {
-                return 0;
-            } else {
-                return -1;
-            }
-        }
+    public static boolean[] check = new boolean[1001];
 
 
-    }
+    public static ArrayList<Integer> dfsResult = new ArrayList<>();
 
+    public static ArrayList<Integer> bfsResult = new ArrayList<>();
 
-    public static void input() throws IOException {
-
-        n = Integer.parseInt(br.readLine());
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int w = Integer.parseInt(st.nextToken());
-            int h = Integer.parseInt(st.nextToken());
-            int num = i + 1;
-
-            list.add(new Person(w, h, num));
-
-        }
-    }
-
-    public static void solve() {
-
-//        list.sort(Comparator.reverseOrder());
-
-        for (int i = 0; i < n; i++) {
-            int downCount = 0; // 나보다 큰 애들 개수
-            for (int j = 0; j < n; j++) {
-                if( i == j )
-                    continue;
-                int temp = validation(list.get(i), list.get(j));
-               if (temp == -1) {
-                   downCount++;
-               }
-            }
-
-            map.put(list.get(i).num, downCount + 1);
-
-        }
-    }
-
-    public static void output() throws IOException {
-        for (int i = 1; i <= n; i++) {
-            bw.write(String.valueOf(map.get(i)) + " ");
-        }
-        bw.flush();
-    }
+    public static Queue<Integer> q = new LinkedList<>();
 
     public static void main(String[] args) throws IOException {
+
         input();
         solve();
         output();
     }
 
-    public static int validation(Person p1, Person p2) {
-        if (p1.weight > p2.weight && p1.height > p2.height) {
-            return 1;
-        } else if (p1.weight == p2.weight && p1.height == p2.height) {
-            return 0;
-        } else if (p1.weight < p2.weight && p1.height < p2.height) {
-            return -1;
-        } else {
-            return -100;
-        }
+    public static void input() throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        v = Integer.parseInt(st.nextToken());
 
+        for(int i = 0; i < m; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            arr[a][b] = true;
+            arr[b][a] = true;
+
+        }
 
     }
 
+    public static void solve() {
+        dfs(v);
+        bfs(v);
+    }
 
+    public static void dfs(int idx){
 
+        check[idx] = true;
+        dfsResult.add(idx);
 
+        for(int i = 1; i <= n; i++){
+            if (arr[idx][i] == true) {
+                if(check[i] == false){
+                    check[i] = true;
+                    dfs(i);
+                }
+            }
+        }
+    }
+
+    public static void bfs(int idx) {
+
+        Arrays.fill(check, false);
+
+        check[idx] = true;
+        q.add(idx);
+
+        while(q.size() > 0){
+            int cur = q.poll();
+            bfsResult.add(cur);
+
+            for(int i = 1; i <= n; i++){
+                if(arr[cur][i] == true && check[i] == false){
+                    check[i]= true;
+                    q.add(i);
+                }
+            }
+        }
+    }
+
+    public static void output() throws IOException {
+        for (int i = 0; i < dfsResult.size(); i++) {
+            bw.write(String.valueOf(dfsResult.get(i)) + " ");
+        }
+
+        bw.write("\n");
+
+        for (int i = 0; i < bfsResult.size(); i++) {
+            bw.write(String.valueOf(bfsResult.get(i)) + " ");
+        }
+
+        bw.flush();
+    }
 
 
 }
