6ce944e2ce45c22fb15dbb9198e294816730f389
hoyo1744 <hoyo1744@naver.com>
2025-04-20 16:48:03 +0900

    스택 [BT] problem/10808 [RV] N/A - 자료구조

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,6 @@ package org.example;
 
 
 import java.util.*;
-import java.util.stream.*;
 import java.io.*;
 
 public class Main {
@@ -11,40 +10,57 @@ public class Main {
 
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+    public static int n;
 
-    public static Map<Character, Integer> map = new HashMap<>();
-
-
+    public static Stack<Integer> stack = new Stack<>();
 
     public static void main(String[] args) throws IOException {
 
-        String str = br.readLine();
+        solve();
+    }
 
-        for (int i = 0; i < str.length(); i++) {
-            if (map.get(str.charAt(i)) == null) {
-                map.put(str.charAt(i), 1);
-            } else {
-                int count = map.get(str.charAt(i));
-                map.put(str.charAt(i), count+1);
+    public static void solve() throws IOException {
+        n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            String cmd = st.nextToken();
+
+            if (cmd.equals("push")) {
+                int num = Integer.parseInt(st.nextToken());
+                stack.push(num);
+
+            } else if (cmd.equals("pop")) {
+                if (stack.size() > 0) {
+                    int pop = stack.pop();
+                    bw.write(String.valueOf(pop)+"\n");
+                } else {
+                    bw.write("-1\n");
+                }
+            } else if (cmd.equals("size")) {
+                bw.write(String.valueOf(stack.size()) + "\n");
+            } else if (cmd.equals("empty")) {
+                if (stack.size() > 0) {
+                    bw.write("0\n");
+                } else {
+                    bw.write("1\n");
+                }
+            } else if (cmd.equals("top")) {
+                if (stack.size() > 0) {
+                    bw.write(String.valueOf(stack.peek()) + "\n");
+                } else {
+                    bw.write("-1\n");
+                }
             }
-        }
-
-
-        for (int i = 'a'; i <= 'z'; i++) {
-            if (map.get((char)i) == null) {
-                bw.write("0" + " ");
-            } else {
-                bw.write(String.valueOf(map.get((char)i)) + " ");
-            }
 
         }
-
-
-
-
 
         bw.flush();
+
     }
+
+
 
 
 }
