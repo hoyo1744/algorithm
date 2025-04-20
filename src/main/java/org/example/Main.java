eaf42b9e64cefc5a81cfc6007a196cf3d91f0aa4
hoyo1744 <hoyo1744@naver.com>
2025-04-20 16:35:00 +0900

    알파벳 개수 [BT] problem/10808 [RV] N/A - 문자열

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+
 import java.util.*;
 import java.util.stream.*;
 import java.io.*;
@@ -11,26 +12,38 @@ public class Main {
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 
+    public static Map<Character, Integer> map = new HashMap<>();
+
+
+
     public static void main(String[] args) throws IOException {
 
         String str = br.readLine();
-        int len = str.length();
 
-        int startIndex = 0;
-        while (true) {
-            if (startIndex + 9 < len) {
-                bw.write(str.substring(startIndex, startIndex + 10) + "\n");
+        for (int i = 0; i < str.length(); i++) {
+            if (map.get(str.charAt(i)) == null) {
+                map.put(str.charAt(i), 1);
             } else {
-                bw.write(str.substring(startIndex) + "\n");
-                break;
+                int count = map.get(str.charAt(i));
+                map.put(str.charAt(i), count+1);
             }
+        }
 
-            startIndex += 10;
+
+        for (int i = 'a'; i <= 'z'; i++) {
+            if (map.get((char)i) == null) {
+                bw.write("0" + " ");
+            } else {
+                bw.write(String.valueOf(map.get((char)i)) + " ");
+            }
 
         }
 
-        bw.flush();
 
+
+
+
+        bw.flush();
     }
 
 
