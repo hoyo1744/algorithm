62734fb70546c89f61325119eda98716ba7bcc88
hoyo1744 <hoyo1744@naver.com>
2025-04-20 16:21:04 +0900

    열 개씩 끊어 출력하기 [BT] problem/11721 [RV] N/A - 문자열

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,6 @@ import java.util.*;
 import java.util.stream.*;
 import java.io.*;
 
-
 public class Main {
 
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,46 +11,27 @@ public class Main {
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 
-
-
     public static void main(String[] args) throws IOException {
 
-        while(true){
-            int num = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+        int len = str.length();
 
-            if (num == 0) {
-                break;
-            }
-
-            solve(num);
-        }
-
-    }
-
-    public static void solve(int num) throws IOException {
-        String str = String.valueOf(num);
-
-        int left = 0;
-        int right = str.length() - 1;
-
-        boolean isPalindrome = true;
-
-        while (left < right) {
-            if (str.charAt(left) == str.charAt(right)) {
-                left++;
-                right--;
+        int startIndex = 0;
+        while (true) {
+            if (startIndex + 9 < len) {
+                bw.write(str.substring(startIndex, startIndex + 10) + "\n");
             } else {
-                isPalindrome = false;
+                bw.write(str.substring(startIndex) + "\n");
                 break;
             }
-        }
 
-        if (isPalindrome) {
-            bw.write("yes\n");
-        } else {
-            bw.write("no\n");
+            startIndex += 10;
+
         }
 
         bw.flush();
+
     }
+
+
 }
