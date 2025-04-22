0c6101e75aeb2e1a2aec8930e0a85f33d8339da0
hoyo1744 <hoyo1744@naver.com>
2025-04-22 23:17:41 +0900

    미로 탐색 [BT] problem/2178 [RV] N/A - bfs

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,91 +8,93 @@ import java.io.*;
 
 public class Main {
 
+    public static char[][] arr = new char[101][101];
+
+    public static boolean[][] check = new boolean[101][101];
+
+    public static int n;
+
+    public static int m;
+
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static HashMap<Character, Integer> data = new HashMap<>();
+    public static int result;
 
-    public static String str;
+    public static int[] dx = {1, 0, -1, 0};
+    public static int[] dy = {0, 1, 0, -1};
 
-    public static String result;
+    static class Pair {
+        public int x;
+        public int y;
+
+        public int d;
+
+        public  Pair(int x, int y, int d) {
+            this.x = x;
+            this.y = y;
+            this.d = d;
+
+        }
+    }
+
 
     public static void main(String[] args) throws IOException {
-
         input();
         solve();
         output();
-
-    }
-
-    public static void solve(){
-        StringBuilder sb = new StringBuilder();
-
-        int count = 0;
-        char latchar = 0;
-
-        for (int i = 'A'; i <= 'Z'; i++) {
-            if (data.get((char) i) == null) {
-                continue;
-            } else {
-                if (data.get((char) i) % 2 == 0) {
-                    // 짝수
-
-                    int loop = data.get((char) i) / 2;
-                    for (int j = 0; j < loop; j++) {
-                        sb.append((char)i);
-                    }
-
-                } else{
-                    if (count == 1) {
-                        return;
-                    }
-                    // 홀수
-                    count += 1;
-                    latchar = (char)i;
-
-                    int loop = data.get((char) i) / 2;
-                    for (int j = 0; j < loop; j++) {
-                        sb.append((char)i);
-                    }
-                }
-
-            }
-        }
-
-        if (count == 1) {
-            // 홀수
-            result = sb.toString() + latchar + sb.reverse().toString();
-        } else {
-            // 짝수
-            result = sb.toString() + sb.reverse().toString();
-
-        }
-
-
     }
 
     public static void input() throws IOException {
-        str = br.readLine();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < str.length(); i++) {
-            if (data.get(str.charAt(i)) == null) {
-                data.put(str.charAt(i), 1);
-            } else {
-                data.put(str.charAt(i), data.get(str.charAt(i)) + 1);
+        for (int i = 1; i <= n; i++) {
+            String line = "0" + br.readLine();
+            arr[i] = line.toCharArray();
+        }
+
+    }
+
+    public static void solve() {
+        Queue<Pair> q = new LinkedList<>();
+
+        q.add(new Pair(1,1,1));
+        check[1][1] = true;
+
+
+        while (q.size() > 0) {
+            Pair p = q.poll();
+
+            if (p.x == n && p.y == m) {
+                result = p.d;
+                break;
             }
+
+            for (int i = 0; i < 4; i++) {
+                int nx = p.x + dx[i];
+                int ny = p.y + dy[i];
+
+                if (nx >= 1 && nx <= n && ny >= 1 && ny <= m) {
+                    if (check[nx][ny] == false && arr[nx][ny] == '1') {
+                        check[nx][ny] = true;
+                        q.add(new Pair(nx, ny, p.d + 1));
+                    }
+
+                }
+
+            }
+
+
+
         }
     }
 
     public static void output() throws IOException {
-        if (result == null) {
-            result = "I'm Sorry Hansoo";
-        }
-
-        bw.write(result);
+        bw.write(String.valueOf(result));
         bw.flush();
-
     }
 
 
