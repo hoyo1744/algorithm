47381fb82a36fbce61cb26a8b051476b2dec4679
hoyo1744 <hoyo1744@naver.com>
2025-05-03 15:57:01 +0900

    구간 합 구하기 4 [BT] problem/11659 [RV] N/A 구간합

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,54 +2,60 @@ package org.example;
 
 
 import java.util.*;
-import java.util.stream.*;
 import java.io.*;
-
-
+import java.util.stream.*;
 public class Main {
+
 
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+    public static int[] arr = new int[100_001];
+
+    public static int[] sum = new int[100_001];
+
+
     public static int n;
 
-    public static int[] dp = new int[1000005];
+    public static int m;
 
-    public static int solve(int num) {
-
-        if (num == 1) {
-            return 0;
-        }
-
-        int ret = dp[num];
-        if (ret != Integer.MAX_VALUE) {
-            return ret;
-        }
-
-
-        if (num % 3 == 0) {
-            ret = Math.min(ret, solve(num/3) + 1);
-        }
-        if (num % 2 == 0) {
-            ret = Math.min(ret, solve(num/2) + 1);
-        }
-
-        ret = Math.min(ret, solve( num - 1) + 1);
-
-        return dp[num] = ret;
-    }
 
     public static void main(String[] args) throws IOException {
+        
+        input();
+        solve();
+        output();
+    }
 
+    public static void input() throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        n = Integer.parseInt(br.readLine());
+        String number = br.readLine();
+        st = new StringTokenizer(number);
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
-        Arrays.fill(dp, Integer.MAX_VALUE);
+    }
 
-        int result = solve(n);
+    public static void solve() throws IOException {
+        for (int i = 1; i <= n; i++) {
+            sum[i] = sum[i-1] + arr[i];
+        }
+    }
 
-        bw.write(String.valueOf(result));
+    public static void output() throws IOException {
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            bw.write(String.valueOf(sum[end]-sum[start-1]) + "\n");
+        }
+
         bw.flush();
     }
 
