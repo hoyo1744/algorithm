3596c3889a371149bb4215ddd59fc62eda814e27
hoyo1744 <hoyo1744@naver.com>
2025-05-03 16:54:57 +0900

    구간 합 구하기 5 [BT] problem/11660 [RV] N/A 구간합 + dp

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,8 @@
 package org.example;
 
-
+import java.util.stream.*;
 import java.util.*;
 import java.io.*;
-import java.util.stream.*;
 public class Main {
 
 
@@ -11,53 +10,62 @@ public class Main {
 
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static int[] arr = new int[100_001];
-
-    public static int[] sum = new int[100_001];
-
-
     public static int n;
 
     public static int m;
 
+    public static int[][] arr = new int[2000][2000];
+
+    public static int[][] dp = new int[2000][2000];
+
 
     public static void main(String[] args) throws IOException {
-        
         input();
         solve();
         output();
     }
 
-    public static void input() throws IOException {
+    public static void input() throws IOException  {
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        String number = br.readLine();
-        st = new StringTokenizer(number);
-        for (int i = 1; i <= n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
+        int count = 1;
 
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= n; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
     }
 
-    public static void solve() throws IOException {
-        for (int i = 1; i <= n; i++) {
-            sum[i] = sum[i-1] + arr[i];
+    public static void solve() {
+        for(int i = 1; i <= n; i++){
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1] + arr[i][j] - dp[i-1][j-1];
+            }
         }
     }
 
     public static void output() throws IOException {
-        for (int i = 0; i < m; i++) {
+        for(int i = 1; i <= m; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
 
-            bw.write(String.valueOf(sum[end]-sum[start-1]) + "\n");
+            int value = dp[x2][y2] + dp[x1-1][y1-1] - dp[x1-1][y2] - dp[x2][y1-1];
+
+            bw.write(String.valueOf(value) + "\n");
         }
 
         bw.flush();
+
     }
+
 
 
 }
