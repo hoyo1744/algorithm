d3b2557b2dc1b1b8f8d5451b332410c40b6b3c03
hoyo1744 <hoyo1744@naver.com>
2025-05-05 14:23:52 +0900

    부분합 [BT] problem/1806 [RV] N/A 누적합 + 투포인터

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,23 +8,24 @@ import java.io.*;
 public class Main {
 
 
-
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-
     public static int n;
-    public static int m;
 
-    public static int result;
 
-    public static int[] arr = new int[10005];
+    public static int s;
 
-    public static int[] sum = new int[10005];
+    public static int result = Integer.MAX_VALUE;
+
+    public static int[] arr = new int[100_000];
+
+    public static int[] sum = new int[100_000];
 
 
     public static void main(String[] args) throws IOException {
+
         input();
         solve();
         output();
@@ -34,7 +35,7 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        s = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
 
@@ -42,39 +43,44 @@ public class Main {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-
         for (int i = 1; i <= n; i++) {
             sum[i] = sum[i-1] + arr[i];
         }
     }
 
-    public static void solve() throws IOException {
-
+    public static void solve() {
 
         int start = 1;
-        int end = 1;
+        int end =  1;
 
         while (start <= end && end <= n) {
-            int cmp = sum[end] - sum[start - 1];
-            if (cmp == m) {
-                result++;
-                end++;
-            } else if (cmp < m) {
-                end++;
-            } else {
+            int current = sum[end] - sum[start - 1];
+            if (current >= s) {
+                result = Math.min(result, end - start + 1);
                 start++;
-
                 if (start > end && start <= n) {
                     end = start;
                 }
+            } else if (current < s) {
+                end++;
             }
         }
     }
 
     public static void output() throws IOException {
-        bw.write(String.valueOf(result));
+
+        if (result == Integer.MAX_VALUE) {
+            bw.write("0");
+        } else {
+            bw.write(String.valueOf(result));
+        }
+
         bw.flush();
+
     }
+
+
+
 
 
 
