9a697efdc63c2898b07d0b28749b3a4cdb278713
hoyo1744 <hoyo1744@naver.com>
2025-05-05 13:56:45 +0900

    수들의 합 2 [BT] problem/2003 [RV] N/A 누적합 + 투포인터

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,53 +5,78 @@ import java.util.*;
 import java.util.stream.*;
 import java.io.*;
 
-
 public class Main {
+
+
 
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+
     public static int n;
+    public static int m;
 
-    public static int[] dp = new int[1000005];
+    public static int result;
 
-    public static int solve(int num) {
+    public static int[] arr = new int[10005];
 
-        if (num == 1) {
-            return 0;
-        }
+    public static int[] sum = new int[10005];
 
-        int ret = dp[num];
-        if (ret != Integer.MAX_VALUE) {
-            return ret;
-        }
-
-
-        if (num % 3 == 0) {
-            ret = Math.min(ret, solve(num/3) + 1);
-        }
-        if (num % 2 == 0) {
-            ret = Math.min(ret, solve(num/2) + 1);
-        }
-
-        ret = Math.min(ret, solve( num - 1) + 1);
-
-        return dp[num] = ret;
-    }
 
     public static void main(String[] args) throws IOException {
+        input();
+        solve();
+        output();
+    }
+
+    public static void input() throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
 
-        n = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            sum[i] = sum[i-1] + arr[i];
+        }
+    }
 
-        Arrays.fill(dp, Integer.MAX_VALUE);
+    public static void solve() throws IOException {
 
-        int result = solve(n);
 
+        int start = 1;
+        int end = 1;
+
+        while (start <= end && end <= n) {
+            int cmp = sum[end] - sum[start - 1];
+            if (cmp == m) {
+                result++;
+                end++;
+            } else if (cmp < m) {
+                end++;
+            } else {
+                start++;
+
+                if (start > end && start <= n) {
+                    end = start;
+                }
+            }
+        }
+    }
+
+    public static void output() throws IOException {
         bw.write(String.valueOf(result));
         bw.flush();
     }
+
+
 
 
 }
