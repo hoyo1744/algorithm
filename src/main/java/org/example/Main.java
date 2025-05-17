af69eb93bf5679ea2999d2f0988615de2bb1276f
hoyo1744 <hoyo1744@naver.com>
2025-05-17 16:34:38 +0900

    배열 합치기 [BT] problem/11728 [RV] N/A 구현

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,107 +1,48 @@
 
 package org.example;
-// 2차원 배열의합
+// 배열 합치기
 
 import java.util.*;
-
 import java.util.stream.*;
 import java.io.*;
 
-
 public class Main {
 
-    static class Pair {
-        public int sx;
-        public int sy;
+    public static int a;
 
-        public int ex;
-
-        public int ey;
-
-        public Pair(int sx, int sy, int ex, int ey) {
-            this.sx = sx;
-            this.sy = sy;
-            this.ex = ex;
-            this.ey = ey;
-        }
-    }
-
-    public static int n;
-
-    public static int m;
-
-    public static int k;
-
-    public static int[][] arr = new int[305][305];
-
-    public static int[][] sum = new int[305][305];
+    public static int b;
 
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static List<Pair> list = new ArrayList<>();
-
-    public static List<Integer> result = new ArrayList<>();
-
-    public static void input() throws IOException {
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-
-        for (int i = 1; i <= n; i++) {
-            String str = br.readLine();
-            st = new StringTokenizer(str);
-
-            for (int j = 1; j <= m; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-                sum[i][j] = sum[i-1][j] + sum[i][j-1] - sum[i-1][j-1] +arr[i][j];
-            }
-        }
-
-        k = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < k; i++) {
-            st = new StringTokenizer(br.readLine());
-            list.add(new Pair(
-                    Integer.parseInt(st.nextToken()),
-                    Integer.parseInt(st.nextToken()),
-                    Integer.parseInt(st.nextToken()),
-                    Integer.parseInt(st.nextToken()))
-            );
-        }
-    }
-
-    public static void output() throws IOException {
-
-        for (int i = 0; i < result.size(); i++) {
-            bw.write(String.valueOf(result.get(i)));
-            bw.write("\n");
-        }
-        bw.flush();
-    }
-
-    public static void solve() {
-        for (int i = 0; i < k; i++) {
-            Pair p = list.get(i);
-
-            int x1 = p.ex;
-            int y1 = p.sy - 1;
-
-            int x2 = p.sx - 1;
-            int y2 = p.ey;
-
-            result.add(sum[p.ex][p.ey] - sum[x1][y1] - sum[x2][y2] + sum[p.sx-1][p.sy-1]);
-        }
-
-    }
-
+    public static List<Integer> list = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
-        input();
-        solve();
-        output();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= a; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= b; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+
+
+        list.sort(Comparator.naturalOrder());
+
+        for (int i = 0; i < list.size(); i++) {
+            bw.write(String.valueOf(list.get(i)) + " ");
+        }
+        bw.flush();
+
+
     }
 
 
