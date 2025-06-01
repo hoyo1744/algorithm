54b064e0b5938f23004fc3fca52b3620310985ff
hoyo1744 <hoyo1744@naver.com>
2025-06-01 14:56:12 +0900

    2xn 타일링 [BT] problem/11726 [RV] N/A 다이나믹프로그래밍

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,10 @@
 
 package org.example;
 
-// 피보나치함수
+// 2xN 타일링
 
-import java.util.stream.*;
 import java.util.*;
+import java.util.stream.*;
 import java.io.*;
 
 
@@ -14,47 +14,35 @@ public class Main {
 
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static int t;
-
     public static int n;
 
     public static int result;
 
-    public static int dp[][] = new int[45][2];
+    public static int[] dp = new int[1005];
 
-    public static void init() {
-        result = 0;
+    public static void input() throws IOException {
+        n = Integer.parseInt(br.readLine());
     }
 
     public static void output() throws IOException {
-        bw.write(String.valueOf(dp[n][0]) + " " + String.valueOf(dp[n][1]) + "\n");
+        bw.write(String.valueOf(dp[n]));
         bw.flush();
     }
 
     public static void solve() {
-
-
-        dp[0][0] = 1;
-        dp[1][1] = 1;
-
-        for (int i = 2; i <= 40; i++) {
-            dp[i][0] = dp[i-1][0] + dp[i-2][0];
-            dp[i][1] = dp[i-1][1] + dp[i-2][1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2])%10_007;
         }
-
     }
 
 
     public static void main(String[] args) throws IOException {
+        input();
+        solve();
+        output();
 
-        t = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < t; i++) {
-            init();
-            n = Integer.parseInt(br.readLine());
-            solve();
-            output();
-        }
     }
 
 
