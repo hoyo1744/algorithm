6040e287027a5af528152066b47b4f0e47cf7cb3
hoyo1744 <hoyo1744@naver.com>
2025-06-07 16:57:54 +0900

    계단 오르기 [BT] problem/2579 [RV] N/A 다이나믹프로그래밍

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,7 @@
 
 package org.example;
 
-// 2xN 타일링
-
+// 계단 오르기
 import java.util.*;
 import java.util.stream.*;
 import java.io.*;
@@ -14,37 +13,43 @@ public class Main {
 
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+    public static int[][] dp = new int[305][3];
+
     public static int n;
 
-    public static int result;
-
-    public static int[] dp = new int[1005];
-
-    public static void input() throws IOException {
-        n = Integer.parseInt(br.readLine());
-    }
-
-    public static void output() throws IOException {
-        bw.write(String.valueOf(dp[n]));
-        bw.flush();
-    }
-
-    public static void solve() {
-        dp[1] = 1;
-        dp[2] = 2;
-        for (int i = 3; i <= n; i++) {
-            dp[i] = (dp[i-1] + dp[i-2])%10_007;
-        }
-    }
-
+    public static int[] arr = new int[305];
 
     public static void main(String[] args) throws IOException {
+
         input();
         solve();
         output();
-
     }
 
+    public static void input() throws IOException {
+        n = Integer.parseInt(br.readLine());
 
+        for (int i = 1; i <= n; i++) {
+            int v = Integer.parseInt(br.readLine());
+            arr[i] = v;
+        }
+    }
+
+    public static void solve() {
+        dp[1][1] = arr[1];
+        dp[2][1] = arr[2];
+        dp[2][2] = arr[1] + arr[2];
+
+
+        for (int i = 3; i <= n; i++) {
+                dp[i][1] = Math.max(dp[i-2][2], dp[i-2][1]) + arr[i];
+                dp[i][2] = dp[i-1][1] + arr[i];
+        }
+    }
+
+    public static void output() throws IOException {
+        bw.write(String.valueOf(Math.max(dp[n][1], dp[n][2])));
+        bw.flush();
+    }
 
 }
